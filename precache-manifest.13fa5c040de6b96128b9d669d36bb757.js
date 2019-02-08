7973b89ac538a73f4a2b517ddeb382fe663d56c7
keithw <keithw@midland.com.hk>
2019-02-08 09:47:32 +0800

    FIX: project

---
+++ precache-manifest.13fa5c040de6b96128b9d669d36bb757.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70ce5d7ba749e08c35ea",
+    "url": "/static/js/main.70ce5d7b.chunk.js"
+  },
+  {
+    "revision": "39cd9489a3b7692a4901",
+    "url": "/static/js/1.39cd9489.chunk.js"
+  },
+  {
+    "revision": "6b29512e2d635ba68ab660e9aeed03dd",
+    "url": "/index.html"
+  }
+];
